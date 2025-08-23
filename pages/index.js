89b390fae89cfdce7574102f6f6e7551b2ee4aff
Igor <97802446+IgorSBecker@users.noreply.github.com>
2025-08-23 17:20:23 +0000

    adiciona pages e package

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>Primeiro desafio do curso de tecnologia \n 
+        Compartilhando com todos os colegas pra seguirmos em frente \o/
+    </h1>
+}
+
+export default Home;
